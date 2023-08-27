641bd5c656691f2ea96288d1fc54456ec925155c
taylor.martinez <taylor.swot.2018@gmail.com>
2023-08-26 19:23:15 -0500

    created comments

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// created global variable to be accessed later
+var startButton = document.querySelector("#start-button");
+
+
+// added click event listener 
+startButton.addEventListener("click", function() {
+
+});
