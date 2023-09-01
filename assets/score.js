4953dac455b6ab8a44055e130c20b5c4a23627e6
taylor.martinez <taylor.swot.2018@gmail.com>
2023-09-01 10:08:51 -0500

    created scorepage.html and score.js'

---
+++ assets/score.js
@@ -0,0 +1,3 @@
+// create for loop of scores and create ul with id and then create lis and the do scores of i .initals and scpace .scores of i .scores
+
+var scores = JSON.parse(localStorage.getItem("highscores")) || []
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
 // created global variable to be accessed later
 const startButton = document.getElementById("start-button");
 const nextButton = document.getElementById("next-button");
-var answerButton = document.querySelectorAll("#answer-button");
+var answerButton = document.querySelector("#answer-buttons");
 var timerElement = document.getElementById("timer");
-var questionElement = document.getElementById("question-container");
+var questionElement = document.getElementById("question");
 var startScreen = document.getElementById("start-screen")
+var questionContainer = document.querySelector(".question-container")
+var inputInitials = document.querySelector(".input-initials")
+var userInput = document.getElementById("userInput")
+var submitbtn = document.getElementById("submit-btn");
+var scoreEL = document.getElementById("score")
 
 var count = 60;
 var score = 0;
@@ -12,16 +17,15 @@ var index = 0;
 var timer;
 var timerCount;
 var currentQuestion;
-var chosenQuestion = "";
-var userResponse = "";
+var userArray = [];
 
 // create var for questions index
 var questions = [
-    { question: "What is JavaScript?", responses: [ "A type of font", "A programming language on the Web", "A coffee shop", "None of these"], answer: 1 },
-    { question: "What is a JavaScript function?", responses: [ "block of code used to perform a specific task", "Hyper Text Markup Language", "Another name for a barista", "None of these"], answer: 0 },
-    { question: "What method can be utilized in the debugging process to display data?", responses: [ "blending", "command + P", "console.log", "None of these"], answer: 2 },
-    { question: "What are JavaScript variables?", responses: [ "JavaScript Notation Object", "containers for storing data", "event handlers", "None of these"], answer: 1 },
-    { question: "JavaScript strings are used for which of the following?", responses: [ "embroidery", "to add style", "to store and manipulate data", "None of these"], answer: 2 },
+    { question: "What is JavaScript?", responses: [ "A type of font", "A programming language on the Web", "A coffee shop", "None of these"], answer: "A programming language on the Web"},
+    { question: "What is a JavaScript function?", responses: [ "block of code used to perform a specific task", "Hyper Text Markup Language", "Another name for a barista", "None of these"], answer: "block of code used to perform a specific task" },
+    { question: "What method can be utilized in the debugging process to display data?", responses: [ "blending", "command + P", "console.log", "None of these"], answer: "console.log" },
+    { question: "What are JavaScript variables?", responses: [ "JavaScript Notation Object", "containers for storing data", "event handlers", "None of these"], answer: "containers for storing data" },
+    { question: "JavaScript strings are used for which of the following?", responses: [ "embroidery", "to add style", "to store and manipulate data", "None of these"], answer: "to store and manipulate data" },
 ];
 
 
@@ -36,15 +40,15 @@ function startGame() {
     startButton.setAttribute("style", "display: none")
     startScreen.setAttribute("style", "display: none")
     renderQuestions()
-    renderResponses()
     startCountDown()
-    checkResponse()
 };
 
 // created function to run when the game is over
 function gameOver() {
-    startButton.setAttribute("style", "display: inline")
-    displayScore;
+    questionContainer.style.display="none"
+    clearInterval(timer);
+    inputInitials.classList.replace("hide", "show")
+    scoreEL.textContent=`Your score: ${score}`
 };
 
 // created function for timer to countdown 
@@ -63,48 +67,56 @@ function startCountDown() {
 
 // creates questions that will be rendered on screen
 function renderQuestions() {
-    console.log("Questions are being rendered")}
-    // Navigate through list of questions
-  function navigate(direction) {
-    index = index + direction;
-    // If you try to navigate 'back' from the start
-    // Go to last question
-    if (index < 0) { 
-      index = questions.length - 1; 
-    questionElement.textContent = questions[index].questions
-    // currentQuestion = questions[""];
-    // chosenQuestion = questions[Math.floor(Math.random() * questions.length)];
-    // for (var i=0; i < questions.length; i++) {
-    // }
-    // questionElement = setAttribute("style", "display: inline");
+    if (index === questions.length) { 
+        gameOver()}
+    questionContainer.classList.replace("hide", "show");
+    questionElement.textContent = questions[index].question
+    console.log("questions object",questions[index]);
+    console.log("list of responses", questions[index].responses);
+    // Clears the html by settting innerHTML to an empty string.
+    answerButton.innerHTML = "";
+    for (var i = 0; i < questions[index].responses.length; i++ ) {
+      console.log(questions[index].responses[i]);
+      var btn = document.createElement("button")
+      btn.textContent = questions[index].responses[i];
+      btn.setAttribute("class", "answer-button")
+    answerButton.append(btn);
     }
-};
-
-// created function for response options to be rendered to page
-function renderResponses() {
-    console.log("Responses are being rendered");
-    // answerButton.setAttribute("style", "display: inline");
-};
+}
 
 // created function to check if user response is correct/incorrect
-function checkResponse() {
+function checkResponse(userResponse) {
     console.log("response is checked");
         if (userResponse == questions[index].answer) {
         score += 25;
-    } else if (userResponse !== questions[index].answer) {
+        index++
+        renderQuestions()
+    } else {
         timerCount-10;
+        index++
+        renderQuestions()
     }
 };
 
+function storage(initials) {
+    if (initials !== ""){
+        userArray = JSON.parse(localStorage.getItem("highscores")) || []
+        var user = {
+            initials:initials,
+            score:score,
+        }
+        userArray.push(user)
+        localStorage.setItem("highscores", JSON.stringify(userArray))
+        window.location.assign("scorepage.html")
+    }
+};
 
+answerButton.addEventListener("click", ()=>{
+    var userChoice = this.event.target.textContent
+    checkResponse(userChoice);
+})
 
-
-
-// check what is clicked on and make sure its a button 
-// check what answer is clicked and then compare it to the right answer
-// if correct
-// call navigate function
-
-// navigate (0);
-// nextButton.addEventListener("click", function(event)) {
-//     event.stopPropagation();
+submitbtn.addEventListener("click", ()=>{
+    var userInitials = userInput.value
+    storage(userInitials)
+} )
